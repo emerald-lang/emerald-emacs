dca683e9b42a0efab6e3d9daad3841cbabeab815
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2016-12-19 23:23:51 -0500

    Updated html tags.

--- emerald-mode.el
+++ emerald-mode.el
@@ -27,14 +27,15 @@
              "command" "datalist" "dd" "del" "details" "dialog" "dfn" "dir"
              "div" "dl" "dt" "em" "embed" "fieldset" "figure" "font" "footer"
              "form" "frame" "frameset" "h1" "h2" "h3" "h4" "h5" "h6" "head"
-             "header" "hgroup" "hr" "html" "i" "iframe" "img" "input" "ins"
-             "keygen" "kbd" "label" "legend" "li" "link" "map" "main" "mark"
-             "menu" "meta" "metas" "meter" "nav" "noframes" "noscript" "object"
-             "ol" "optgroup" "option" "output" "p" "param" "pre" "progress" "q"
-             "rp" "rt" "ruby" "s" "samp" "script" "scripts" "section" "select"
-             "small" "source" "span" "strike" "strong" "style" "styles" "sub"
-             "sup" "table" "tbody" "td" "textarea" "tfoot" "th" "thead" "time"
-             "title" "tr" "tt" "u" "ul" "var" "video" "xmp") 'words))
+             "header" "hgroup" "hr" "html" "i" "iframe" "img" "images" "input"
+             "ins" "keygen" "kbd" "label" "legend" "li" "link" "map" "main"
+             "mark" "menu" "meta" "metas" "meter" "nav" "noframes" "noscript"
+             "object" "ol" "optgroup" "option" "output" "p" "param" "pre"
+             "progress" "q" "rp" "rt" "ruby" "s" "samp" "script" "scripts"
+             "section" "select" "small" "source" "span" "strike" "strong"
+             "style" "styles" "sub" "sup" "table" "tbody" "td" "textarea"
+             "tfoot" "th" "thead" "time" "title" "tr" "tt" "u" "ul" "var"
+             "video" "xmp") 'words))
   "Regex for html tags.")
 
 (defconst emerald-selfclosing-tags-re
